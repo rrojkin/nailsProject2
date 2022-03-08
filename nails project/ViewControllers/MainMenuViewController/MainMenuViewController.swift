cc1285872fbf81372cd23016b5a92aa9fe609840
Anton Razhnou <toxihrojkih@gmail.com>
2022-03-08 15:38:05 +0300

    added datePickerController and button pushing it

--- nails project/ViewControllers/MainMenuViewController/MainMenuViewController.swift
+++ nails project/ViewControllers/MainMenuViewController/MainMenuViewController.swift
@@ -16,14 +16,11 @@ class MainMenuViewController: UIViewController {
     }
 
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func onlineRegistrationAction(_ sender: Any) {
+        let datePickerVC = DatePickerViewController(nibName: String(describing: DatePickerViewController.self), bundle: nil)
+        navigationController?.pushViewController(datePickerVC, animated: true)
+        
     }
-    */
+    
 
 }
